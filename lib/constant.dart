561cec3ae6f95d37cbd11e7b21189c40faaa7232
Dafa Raisya <dafaraisya.dr@gmail.com>
2021-08-09 18:23:54 +0700

    add sign in class

---
+++ lib/constant.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF14213D);
+const kPrimaryLightColor = Color(0xFFFFECDF);
+const kPrimaryGradientColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color(0xFFFFA53E), Color(0xFFFF7643)],
+);
+const kSecondaryColor = Color(0xFF979797);
+const kTextColor = Color(0xFFFF652C);
+const kDefaultPadding = 20.0;
+const kErrorColor = Color(0xFFF03738);
+const kContentColorLightTheme = Color(0xFF1D1D35);
+const kHeaderText = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+  fontSize: 20,
+  fontFamily: 'Montserrat Regular'
+);
+const kSubHeaderText = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+  fontSize: 16,
+  fontFamily: 'Montserrat Regular'
+);
+const kParagraphText = TextStyle(
+  color: Colors.black,
+  fontSize: 14,
+  fontFamily: 'Montserrat Regular'
+);
+
+const kAnimationDuration = Duration(milliseconds: 200);
+
+const defaultDuration = Duration(milliseconds: 250);
+
+// Form Error
+final RegExp emailValidatorRegExp = RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+const String kEmailNullError = "Please Enter your email";
+const String kInvalidEmailError = "Please Enter Valid Email";
+const String kUsernameNullError = "Please Enter your Username";
+const String kInvalidUsernameError = "Please Enter your Username";
+const String kPassNullError = "Please Enter your password";
+const String kShortPassError = "Password is too short";
+const String kMatchPassError = "Passwords don't match";
+const String kNamelNullError = "Please Enter your name";
+const String kPhoneNumberNullError = "Please Enter your phone number";
+const String kAddressNullError = "Please Enter your address";
+const String kStatusNullError = "Please Enter your status";
+const String kNikNullError = "Please Enter your NIK";
+const String kNullError = "harus di isi";
---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:rongsokin_pengepul/screens/sign_in/sign_in.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SignIn(),
+    );
+  }
+}
