526c980beddf45b3e27cc2841dbb7f64b25c2fa2
bakarbsa <abubakar.bsa30@gmail.com>
2021-08-10 15:06:22 +0700

    Add Confirmation Pickup Screen

--- lib/screens/transaction/confirmation_pickup.dart
+++ lib/screens/transaction/confirmation_pickup.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:rongsokin_pengepul/components/default_appBar.dart';
 import 'package:rongsokin_pengepul/components/default_navBar.dart';
+import 'package:intl/intl.dart';
 
 class ConfirmationPickUp extends StatefulWidget {
   const ConfirmationPickUp({Key? key}) : super(key: key);
@@ -15,6 +16,264 @@ class _ConfirmationPickUpState extends State<ConfirmationPickUp> {
     return Scaffold(
       appBar: DefaultAppBar(),
       bottomNavigationBar: DefaultNavBar(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: InkWell(
+        onTap: () {},
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Container(
+            height: 60,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              color: Color(0xFFFFC233),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Center(
+              child: Text(
+                'Ambil Sekarang',
+                style: TextStyle(
+                  color: Color(0xFF1D438A),
+                  fontFamily: 'Montserrat',
+                  fontSize: 22,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // Detail User Text
+            Text(
+              'DETAIL USER',
+              style: TextStyle(
+                color: Color(0xFF163570),
+                fontFamily: 'Montserrat',
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            //Detail User Container
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  height: 120,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFFFC233),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 40, right: 40, top: 20, bottom: 15),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Pasha Ungu',
+                              style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.phone,
+                                  size: 26,
+                                ),
+                                SizedBox(width: 3),
+                                Text(
+                                  '+6281658737356',
+                                  style: TextStyle(
+                                    fontFamily: 'Montserrat',
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                        Spacer(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.location_on,
+                              size: 27,
+                            ),
+                            SizedBox(width: 7),
+                            Flexible(
+                              child: Text(
+                                'Jl. Teknik Kimia, Keputih, Kec. Sukolilo, Kota SBY, Jawa Timur 60111',
+                                style: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 14,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Container(
+                    height: 2,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Text(
+              'DETAIL BARANG',
+              style: TextStyle(
+                color: Color(0xFF163570),
+                fontFamily: 'Montserrat',
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              height: MediaQuery.of(context).size.height - 470,
+              child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return ItemListCard();
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ItemListCard extends StatefulWidget {
+  const ItemListCard({Key? key}) : super(key: key);
+
+  @override
+  _ItemListCardState createState() => _ItemListCardState();
+}
+
+class _ItemListCardState extends State<ItemListCard> {
+  bool _isChecked = false;
+  int price = 10000;
+  int weight = 1;
+  var currency = new NumberFormat.simpleCurrency(locale: 'id_ID');
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 5, vertical: 20),
+        child: ListTile(
+          leading: Checkbox(
+            checkColor: Colors.white,
+            activeColor: Color(0xFFFFC233),
+            value: _isChecked,
+            onChanged: (bool? value) {
+              setState(() {
+                _isChecked = value!;
+              });
+            },
+          ),
+          title: Text(
+            'Botol Kaca',
+            style: TextStyle(
+              fontFamily: 'Montserrat',
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          subtitle: Text(
+            'Barang kondisi baik hanya goresan dikit',
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'Montserrat',
+              fontSize: 12,
+            ),
+          ),
+          trailing: Container(
+            width: 140,
+            child: Column(
+              children: [
+                Text(
+                  '${currency.format(price * weight)}',
+                  style: TextStyle(
+                    fontFamily: 'Montserrat',
+                    fontSize: 16,
+                  ),
+                ),
+                SizedBox(height: 5),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        setState(() {
+                          weight <= 1 ? 1 : weight--;
+                        });
+                      },
+                      child: Container(
+                        height: 25,
+                        width: 25,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: Center(child: Icon(Icons.remove)),
+                      ),
+                    ),
+                    Text(
+                      weight.toString() + ' Kg',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 15,
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        setState(() {
+                          weight++;
+                        });
+                      },
+                      child: Container(
+                        height: 25,
+                        width: 25,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: Center(child: Icon(Icons.add)),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
